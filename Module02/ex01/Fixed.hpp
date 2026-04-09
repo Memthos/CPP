51e9e79ede6b9dddb85d64c6c0a79f88de00cff6
Memthos <perrinetrebi.m@gmail.com>
2026-04-09 18:06:06 +0200

    added base files

---
+++ Module02/ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef __FIXED_H__
+# define __FIXED_H__
+
+#include <iostream>
+#
+
+class	Fixed
+{
+	public:
+		Fixed();
+		Fixed(const int value);
+		Fixed(const float value);
+		Fixed(const Fixed& input);
+		Fixed&	operator=(const Fixed& input);
+		~Fixed();
+		float	toFloat(void) const;
+		int		toInt(void) const;
+		int		getRawBits(void) const;
+		void	setRawBits(const int raw);
+		private:
+		int					_nbValue;
+		static const int	_nbFracBits;
+	};
+
+std::ostream&	operator<<(std::ostream &stream, const Fixed &value);
+
+#endif

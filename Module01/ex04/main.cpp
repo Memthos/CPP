3571c6d05023d8d01800115dc2752816104373d5
Memthos <perrinetrebi.m@gmail.com>
2026-05-01 12:34:04 +0200

    fix module01 ex04 for null arg and same args

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -20,7 +20,7 @@ int	main(int argc, char **argv)
 	size_t			idx;
 	const char		*name;
 
-	if (argc != 4)
+	if (argc != 4 || argv[2][0] == '\0')
 		return (1);
 	infile.open(argv[1]);
 	name = get_outName(argv[1]);
@@ -37,13 +37,16 @@ int	main(int argc, char **argv)
 	while (infile)
 	{
 		std::getline(infile, str);
-		idx = str.find(argv[2]);
-		while (idx != std::string::npos)
+		if (std::strcmp(argv[2], argv[3]) != 0)
 		{
-			str.erase(idx, strlen(argv[2]));
-			str.insert(idx, argv[3]);
 			idx = str.find(argv[2]);
+			while (idx != std::string::npos)
+			{
+				str.erase(idx, strlen(argv[2]));
+				str.insert(idx, argv[3]);
+				idx = str.find(argv[2]);
 
+			}
 		}
 		outfile << str << std::endl;
 	}

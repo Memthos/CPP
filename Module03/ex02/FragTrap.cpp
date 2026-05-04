7c2fbd8b89a1ccfc93f37ea08d66e5a070b031a4
Memthos <perrinetrebi.m@gmail.com>
2026-05-04 11:03:31 +0200

    change ex02 inheritance from ScavTrap to ClapTrap

--- Module03/ex02/FragTrap.cpp
+++ Module03/ex02/FragTrap.cpp
@@ -1,13 +1,14 @@
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap(std::string name) : ScavTrap(name)
+FragTrap::FragTrap(std::string name) : ClapTrap(name)
 {
+	_health = 100;
 	_energy = 100;
 	_damage = 30;
 	std::cout << "FragTrap " << name << " constructor called" << std::endl;
 }
 
-FragTrap::FragTrap(const FragTrap &input) : ScavTrap(input)
+FragTrap::FragTrap(const FragTrap &input) : ClapTrap(input)
 {
 	std::cout << "FragTrap " << input._name << " copy constructor called" << std::endl;
 }
@@ -16,7 +17,7 @@ FragTrap	FragTrap::operator=(const FragTrap &input)
 {
 	if (this != &input)
 	{
-		ScavTrap::operator=(input);
+		ClapTrap::operator=(input);
 	}
 	std::cout << "FragTrap " << input._name << " copy assignment operator called" << std::endl;
 	return (*this);

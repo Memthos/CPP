6a2fde0c643fbc6f990bfa041cc3b6611cd65050
Memthos <perrinetrebi.m@gmail.com>
2026-05-07 17:27:33 +0200

    add missing files in module03 ex02

---
+++ Module03/ex02/ScavTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef SCAV_TRAP_H
+# define SCAV_TRAP_H
+
+# include "ClapTrap.hpp"
+# include <iostream>
+# include <string>
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap& input);
+		ScavTrap&	operator=(const ScavTrap& input);
+		~ScavTrap();
+		void		attack(const std::string& target);
+		void		guardGate(void);
+};
+
+#endif
